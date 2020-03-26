b68b085d0fce921bb3505c722279c02000b2f41f
Luis Novais <lnovais9@hotmail.com>
2020-03-26 23:31:12 +0000

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-PT';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
